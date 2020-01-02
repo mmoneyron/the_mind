524d254efdca148d8b539c812e8e9778e61a673e
mmoneyron <mathieu.moneyron44@gmail.com>
2020-01-02 20:17:03 +0100

    WIP

--- public/disp.js
+++ public/disp.js
@@ -1,9 +1,9 @@
 // Create the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 800;
-//document.body.appendChild(canvas);
+canvas.width = 1004;
+canvas.height = 752;
+document.body.appendChild(canvas);
 
 // Background image
 var bgReady = false;
@@ -11,68 +11,144 @@ var bgImage = new Image();
 bgImage.onload = function () {
         bgReady = true;
 };
-bgImage.src = "./background.png";
+bgImage.src = "./pictures/Images/background.png";
 
-// Background image
-var cardReady = false;
-var cardImage = new Image();
-cardImage.onload = function () {
-        cardReady = true;
+// Card Back
+var cardBackReady = false;
+var cardBack = new Image();
+cardBack.onload = function () {
+        cardBackReady = true;
 };
-cardImage.src = "./pictures/card_back.png";
+cardBack.src = "./pictures/Images/back_card.png";
 
-netText="-";
+// Card
+var cardReady = false;
+var card = new Image();
+card.onload = function () {
+        //cardReady = true;
+};
+canvas.onclick = function () {
+	if (!playerDone) {
+		if (event.offsetX >= 80 && event.offsetX <=250 && event.offsetY >= 200 && event.offsetY <= 490) {
+			console.log("PLAY");
+			socket.emit('game', "PLAY");
+			playerDone= 1;
+		}
+	}
+}
+card.src = "./pictures/Images/card20.png";
+
+// played card 1
+var cardP1Ready = false;
+var cardP1 = new Image();
+var cards = [];
+cards.push(new Image());
+cards[0].onload = function () {
+        //cardP1Ready = true;
+};
+cards[0].src = "./pictures/Images/card12.png";
+
+// played card 2
+var cardP2Ready = false;
+cards.push(new Image());
+cards[1].onload = function () {
+        //cardP2Ready = true;
+};
+cards[1].src = "./pictures/Images/card33.png";
+
+// played card 3
+var cardP3Ready = false;
+cards.push(new Image());
+cards[2].onload = function () {
+        //cardP3Ready = true;
+};
+cards[2].src = "./pictures/Images/card46.png";
+
+// played card 4
+var cardP4Ready = false;
+cards.push(new Image());
+cards[3].onload = function () {
+        //cardP4Ready = true;
+};
+cards[3].src = "./pictures/Images/card94.png";
+
+var nb_players = -1;
+var PlayerID = -1;
+var playersDone = [0, 0, 0, 0];
+var playerDone = 0;
+var j = 0;
 
 var socket = io.connect('http://localhost:8000');
 socket.on('message', function(message) {
-	//alert('Le serveur a un message pour vous : ' + message);
 	netText=message;
+	console.log(message);
+	if (message[0] == 'C') { // Card
+		cards[PlayerID].src = "./pictures/Images/card" + message.split('C')[1] + ".png";
+		cardReady = true;
+	} else if (message[0] == 'N') { // Number of players connected
+		if (nb_players == -1) {
+			PlayerID = parseInt(message.split('N')[1]) - 1;
+			console.log("Player ID : " + PlayerID);
+		}
+		nb_players = parseInt(message.split('N')[1]);
+	} else if (message[0] == 'P') {
+		var n = parseInt(message.split('P')[1]);
+		playersDone[n] = 1;
+	} else if (message[0] == 'A') { // All cards
+		m = message.split('A')[1];
+		a = [];
+		a.push(m.split('.')[0]);
+		a.push(m.split('.')[1]);
+		a.push(m.split('.')[2]);
+		a.push(m.split('.')[3]);
+		//for (var  i = 0; i < 4; i++) {
+			cards[0].src = "./pictures/Images/card"+a[0]+".png";
+			cards[1].src = "./pictures/Images/card"+a[1]+".png";
+			cards[2].src = "./pictures/Images/card"+a[2]+".png";
+			cards[3].src = "./pictures/Images/card"+a[3]+".png";
+		//}
+		console.log(a);
+	}
 	render();
 });
 
 // Draw everything
+var stackX;
+var stackY;
 var render = function () {
-        if (bgReady) {
-                //ctx.drawImage(bgImage, 0, 0);
-        }
-        if (cardReady) {
-                //ctx.drawImage(cardImage, 20, 20);
-        }
-
-        // Score
-	var para = document.createElement("p");
-	var node = document.createTextNode("This is new.");
-	para.appendChild(node);
-
-	var paren = document.getElementById("div1");
-	var child = document.getElementById("div1");
-	element.appendChild(para);
-        ctx.fillStyle = "rgb(250, 250, 250)";
-        ctx.font = "24px Helvetica";
-        ctx.textAlign = "left";
-        ctx.textBaseline = "top";
-        //ctx.fillText(netText, 32, 32);
-	var j = 0;
-	var lines = netText.split('\n');
-	/*while (j < lines.length) {
-        	ctx.fillText(lines[j], 32, 32 + (j*32));
-		j++;
-	}*/
+	if (bgReady) {
+		ctx.drawImage(bgImage, 0, 0, 1004, 752);
+  }
+  if (cardBackReady) {
+  	/*if (playerDone) {
+			ctx.drawImage(cardP1, 450, 180, 120, 180);
+	  } else {
+  	  ctx.drawImage(card, 50, 180, 200, 310);
+		}*/
+		stackX = 450;
+		stackY = 180;
+		var j = 0;
+		for (var i = 0; i < nb_players; i++) {
+			if (!playersDone[i]) {
+				if (i == PlayerID) {
+  	  		ctx.drawImage(cards[i], 50, 180, 200, 310);
+				} else {
+					ctx.drawImage(cardBack, 850, 150+200*j, 120, 180);
+					j++;
+				}
+			} else {
+				ctx.drawImage(cards[i], stackX, stackY, 120, 180);
+				stackX += 50;
+				stackY += 60;
+			}
+		}
+  }
 };
-
-/*
-function animate() {
-        render();
-
-        // Request a new animation frame using Paul Irish's shim
-        window.requestAnimFrame(animate);
-};
-*/
 
 function run() {
-        render();
-        time = Date.now();
-        }
+	render();
+  time = Date.now();
+}
 
 var time = Date.now();
 setInterval(run, 10);
---
+++ the_mind.js
@@ -0,0 +1,115 @@
+/**************************************************
+** NODE.JS REQUIREMENTS
+**************************************************/
+var util = require("util"),
+    express = require("express"),
+        app = express(),
+        http = require("http"),
+	sq = require('sqlite3'),
+        io = require("socket.io")(http);                              // Socket.IO
+
+/**************************************************
+** GAME VARIABLES
+**************************************************/
+var socket;             // Socket controller
+
+var n;
+var nb_players = 0;
+var sclient;
+var prot=0;
+var cards = [];
+var clients = [];
+var playersDone = [0, 0, 0, 0];
+var start = false;
+var played = false
+
+function runGame() {
+  //console.log("------------------------");
+	if (!start) {
+		io.sockets.emit('message', 'N' + nb_players.toString());
+		if (nb_players == 4) {
+			start = true;
+			io.sockets.emit('message', 'A' + cards[0] + '.' + cards[1] + '.' + cards[2] + '.' + cards[3]);
+			console.log(cards);
+			cards = cards.sort();
+		}
+	}
+	if (played) {
+		for (var i = 0; i < 4; i++) {
+			if (playersDone[i]) {
+				io.sockets.emit('message', 'P' + i);
+			}
+		}
+		played = false;
+	}
+}
+
+/**************************************************
+** GAME INITIALISATION
+**************************************************/
+function init() {
+	// Create an empty array to store players
+  server=http.createServer(app);
+  // Set up Socket.IO to listen on port 8000
+  socket = io.listen(server);
+	server.listen(8000);
+  app.use(express.static(__dirname+"/public"));
+
+	// Start listening for events
+  io.on("connection", onSocketConnection);
+  console.log("the_mind.js started");
+	
+	// Build cards array
+	n = Math.floor(Math.random() * 100) + 1;
+	cards.push(n);
+	n = Math.floor(Math.random() * 100) + 1;
+	while (n == cards[0]) {
+		n = Math.floor(Math.random() * 100) + 1;
+	}
+	cards.push(n);
+	n = Math.floor(Math.random() * 100) + 1;
+	while (n == cards[0] || n == cards[1]) {
+		n = Math.floor(Math.random() * 100) + 1;
+	}
+	cards.push(n);
+	n = Math.floor(Math.random() * 100) + 1;
+	while (n == cards[0] || n == cards[1] || n == cards[2]) {
+		n = Math.floor(Math.random() * 100) + 1;
+	}
+	cards.push(n);
+
+	setInterval(runGame, 1000);
+};
+
+function getClientByID(id) {
+	for (var i = 0; i < nb_players; i++) {
+		if (id == clients[i])
+			return i;
+	}
+};
+
+// New socket connection
+function onSocketConnection(client) {
+        //console.log("New player has connected: "+client.constructor.name);
+        console.log("New player has connected: "+client.id);
+
+	if (nb_players < 4) {
+		nb_players++;
+		client.emit('message', 'N' + nb_players.toString());
+		client.emit('message', "C" + cards[nb_players-1].toString());
+		sclient=client;
+		prot=1;
+		clients.push(client.id);
+		if (nb_players == 4) {
+		}
+	}
+	client.on('game',  function(msg) {
+		console.log('[' + getClientByID(client.id) + '] : ' + msg);
+		playersDone[getClientByID(client.id)] = 1;
+		played = true;
+	});
+
+};
+
+
+init();
